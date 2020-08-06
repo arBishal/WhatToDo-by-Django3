87ed246bc5c7aa69dfd99de23bd7434eff696b9d
arBishal <arbishal.319@gmail.com>
2020-08-07 01:47:07 +0600

    Log Out done

--- todo/views.py
+++ todo/views.py
@@ -2,7 +2,12 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.db import IntegrityError
-from django.contrib.auth import login
+from django.contrib.auth import login, logout
+
+
+def home(request):
+    return render(request, 'todo/home.html')
+
 
 
 def signupuser(request):
@@ -27,6 +32,14 @@ def signupuser(request):
             return render(request, 'todo/signupuser.html', {'form':UserCreationForm(), 'error':'Password did not match!'})
 
 
+def logoutuser(request):
+
+    if request.method == 'POST':
+        logout(request)
+        return redirect('home')
+
+
 def currenttodos(request):
+
     return render(request, 'todo/currenttodos.html')
 

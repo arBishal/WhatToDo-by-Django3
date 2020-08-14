345c344e6d4d7951f8dd3b4aadc7571c9630639d
arBishal <arbishal.319@gmail.com>
2020-08-15 01:37:21 +0600

    editing todos done

--- todo/views.py
+++ todo/views.py
@@ -74,15 +74,26 @@ def currenttodos(request):
 
 
 def viewtodo(request, todo_pk):
-    todo = get_object_or_404(Todo, pk=todo_pk)
+    todo = get_object_or_404(Todo, pk=todo_pk, user=request.user)
     form = TodoForm(instance=todo)
     return render(request, 'todo/viewtodo.html', {'todo': todo, 'form':form})
 
 
 def edittodo(request, todo_pk):
-    todo = get_object_or_404(Todo, pk=todo_pk)
-    form = TodoForm(instance=todo)
-    return render(request, 'todo/edittodo.html', {'todo': todo, 'form':form})
+    todo = get_object_or_404(Todo, pk=todo_pk, user=request.user)
+    if request.method == 'GET':
+        form = TodoForm(instance=todo)
+        return render(request, 'todo/edittodo.html', {'todo': todo, 'form': form})
+    else:
+        try:
+            form = TodoForm(request.POST, instance=todo)
+            form.save()
+            return redirect('currenttodos')
+        except ValueError:
+            return render(request, 'todo/edittodo.html', {'todo': todo, 'form': form, 'error': 'Title is too big. Try a new one!'})
+
+
+
 
 
 
